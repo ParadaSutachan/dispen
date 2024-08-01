de4bd9d1baff843305e36e354cb70b168e21f4da
Laps <parada.sutachan@gmail.com>
2024-08-01 12:29:57 -0500

    cambio dir result

--- galga.py
+++ galga.py
@@ -150,7 +150,7 @@ pi.write(motor2_en_pin, 1)
 motor1_speed = 50
 control_motor(motor1_pwm_pin, motor1_dir_pin, motor1_speed, 'forward')
 # Crear el archivo de salida para guardar los datos
-output_file_path = '/home/santiago/Documents/dispensador/dispensador/test_controlador_ss.txt'
+output_file_path = '/home/santiago/Documents/dispensador/dispen/test_controlador_ss.txt'
 with open(output_file_path, 'w') as output_file:
     output_file.write("Tiempo \t PWM \t W \tFlujo \n")
 
