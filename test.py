1db3130dc9ceeab39218450e14eecc20972fcbc2
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-08-03 16:54:13 -0500

    12

--- test.py
+++ test.py
@@ -244,7 +244,7 @@ with open(output_file_path, 'w') as output_file:
         
         # Registrar los datos en el archivo
         ts = time.time() - start_time
-        output_file.write(f"{ts:.2f}\t{upi_s:.2f}\t{W:.2f}\t{fm_n:.2f}\t{wg}\n")
+        output_file.write(f"{ts:.2f}\t{upi_s:.2f}\t{W:.2f}\t{fm_n:.2f}\t{wg}")
 
 
         # Restablecer contadores
