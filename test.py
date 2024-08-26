5522f0b8f24647d1476286d056751a59289d65f1
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-08-26 15:42:23 -0500

    .

--- test.py
+++ test.py
@@ -151,7 +151,7 @@ delta_W_1= 0.0
 delta_fn_1 =0.0
 delta_fn_2 = 0.0
 # Crear el archivo de salida para guardar los datos
-output_file_path = '/home/santiago/Documents/dispensador/dispen/prueba_35_5_45_15.txt'
+output_file_path = '/home/santiago/Documents/dispensador/dispen/prueba.m2.ms.txt'
 with open(output_file_path, 'w') as output_file:
     output_file.write("Tiempo \t PWM \t W \tFlujo \tpeso \n")
 
