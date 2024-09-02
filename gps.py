55719a085d37ea67b66a72c483ce7b6d09fe39cd
Laps <parada.sutachan@gmail.com>
2024-09-02 14:17:31 -0500

    .

--- gps.py
+++ gps.py
@@ -4,10 +4,9 @@ import string
 import pynmea2
 
 while True:
-	port="/dev/ttyAMA0"
-	ser=serial.Serial(port, baudrate=9600, timeout=0.5)
-	dataout = pynmea2.NMEAStreamReader()
-	newdata=ser.readline()
-	
-    if newdata[0:6] == '$GPRMC':
-        print(1)
+        port="/dev/ttyAMA0"
+        ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+        dataout = pynmea2.NMEAStreamReader()
+        newdata=ser.readline()
+        if newdata[0:6] == '$GPRMC':
+            print(1)
