133ba701e7b81ef3966c2c053b6e93232f6ebf9c
Laps <parada.sutachan@gmail.com>
2024-09-02 17:11:05 -0500

    .

--- gps.py
+++ gps.py
@@ -1,30 +1,30 @@
-import serial
-import time
-import string
-import pynmea2
+import serial  
+import time  
+import pynmea2  
 
-while True:
-	port="/dev/ttyAMA0"
-	ser=serial.Serial(port, baudrate=9600, timeout=0.1)
-	dataout = pynmea2.NMEAStreamReader()
-	newdata=ser.readline().decode('utf-8').strip()
-	
-	if newdata[0:6] == "$GPRMC":
-		newmsg=pynmea2.parse(newdata)
-		status = newmsg.status
-		print(status)
-			
-        if (status == "A"):
-            lat=newmsg.latitude
-            lng=newmsg.longitude
-            gps = "Lat = " + str(lat) + " Lng = " + str(lng)
-            print(gps)
-            speed = newmsg.spd_over_grnd
-		    #speed_mps= speed_kmph*(1000/3600)
-            print(f"Speed : {speed:.2f} m/s " )
-	        #speed_mps = speed_kmph * (1000 / 3600)
-            #print(f"Speed (horizontal): {speed_mps:.2f} m/s")
-        elif status == "V":
-            print("Estoy Agarrando Señal Krnal ...")
-            print("Estoy Cansado Jefe")
+# Configura el puerto serie  
+port = "/dev/ttyAMA0"  
+ser = serial.Serial(port, baudrate=9600, timeout=0.1)  
+
+while True:  
+    newdata = ser.readline().decode('utf-8').strip()  
+    
+    # Verifica si se recibe una sentencia GPRMC  
+    if newdata[0:6] == "$GPRMC":  
+        newmsg = pynmea2.parse(newdata)  
+        status = newmsg.status  
+        print(status)  
+        
+        # Maneja los estados A y V  
+        if status == "A":  
+            lat = newmsg.latitude  
+            lng = newmsg.longitude  
+            gps = f"Lat = {lat} Lng = {lng}"  
+            print(gps)  
+            speed = newmsg.spd_over_grnd  # velocidad en nudos  
+            speed_mps = speed * (0.514444)  # convertimos de nudos a m/s  
+            print(f"Speed: {speed:.2f} knots / {speed_mps:.2f} m/s")  
+        elif status == "V":  
+            print("Estoy Agarrando Señal, Krnal ...")  
+            print("Estoy Cansado, Jefe")
 	
