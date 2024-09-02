6bb9d7b7e4ac1f5af47897438e95a14f6ebce623
Laps <parada.sutachan@gmail.com>
2024-09-02 16:55:13 -0500

    .

--- gps.py
+++ gps.py
@@ -3,22 +3,25 @@ import time
 import string
 import pynmea2
 
+port="/dev/ttyAMA0"
+ser=serial.Serial(port, baudrate=9600, timeout=0.1)
+dataout = pynmea2.NMEAStreamReader()
+newdata=ser.readline().decode('utf-8').strip()
+newmsg=pynmea2.parse(newdata)
 while True:
-	port="/dev/ttyAMA0"
-	ser=serial.Serial(port, baudrate=9600, timeout=0.1)
-	dataout = pynmea2.NMEAStreamReader()
-	newdata=ser.readline().decode('utf-8').strip()
-	
-	if newdata[0:6] == "$GPRMC":
-		newmsg=pynmea2.parse(newdata)
-		lat=newmsg.latitude
-		lng=newmsg.longitude
-		gps = "Lat = " + str(lat) + " Lng = " + str(lng)
-		print(gps)
-		speed = newmsg.spd_over_grnd
-		#speed_mps= speed_kmph*(1000/3600)
-		print(f"Speed : {speed:.2f} m/s " )
-		print(newmsg.status)
-	    #speed_mps = speed_kmph * (1000 / 3600)
-        #print(f"Speed (horizontal): {speed_mps:.2f} m/s")
-	
+    if newmsg.status == 'V':
+        print('Estoy Agarrando señal Karnarl...')
+        
+    while newmsg.status == 'A':
+        if newdata[0:6] == "$GPRMC":
+            newmsg=pynmea2.parse(newdata)
+            lat=newmsg.latitude
+            lng=newmsg.longitude
+            gps = "Lat = " + str(lat) + " Lng = " + str(lng)
+            print(gps)
+            speed = newmsg.spd_over_grnd
+            #speed_mps= speed_kmph*(1000/3600)
+            print(f"Speed : {speed:.2f} m/s " )
+            #speed_mps = speed_kmph * (1000 / 3600)
+            #print(f"Speed (horizontal): {speed_mps:.2f} m/s")
+		
