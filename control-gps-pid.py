6bc22c0e7a3176e7b086ad5b047d01468c5fce2c
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-09-03 14:35:02 -0500

    beta-test_completo

--- control-gps-pid.py
+++ control-gps-pid.py
@@ -1,15 +1,23 @@
 # -- coding: utf-8 --
 #!/usr/bin/env python3
-import time
+
+import serial
+import time  
+import pynmea2
+import shapefile
+from shapely.geometry import shape, Point
 import pigpio
 import RPi.GPIO as GPIO
 import math
-import serial
 from pytictoc import TicToc
 
 # Inicialización de Pigpio
 pi = pigpio.pi()
 pi_m = math.pi
+
+# Configura el puerto serie  
+port = "/dev/ttyAMA0"  
+ser = serial.Serial(port, baudrate=9600, timeout=0.1) 
 
 # Configuración de pines de motor y encoder
 motor1_pwm_pin = 12
@@ -45,7 +53,6 @@ W=0.0
 W2= 0.0
 
 # Variables control maestro esclavo
-
 Kp_m = 0.049398
 ki_m = 0.241
 kp_s = 0.36612
@@ -60,7 +67,7 @@ rk_m= 0.0
 yk_m= 0.0
 ek_m= 0.0
 iek_m= 0.0
-iek_m_1= 0.0    #PARA M1
+iek_m_1= 0.0    # PARA M1
 upi_m= 0.0
 up_m = 0.0
 ui_m = 0.0
@@ -68,7 +75,7 @@ ui_m = 0.0
 rk_m2= 0.0
 yk_m2= 0.0
 ek_m2= 0.0
-iek_m2= 0.0     #PARA M2
+iek_m2= 0.0     # PARA M2
 iek_m_12= 0.0
 upi_m2= 0.0
 up_m2 = 0.0
@@ -78,7 +85,7 @@ rk_s= 0.0
 yk_s= 0.0
 ek_s= 0.0
 iek_s= 0.0
-iek_s_1= 0.0    #PARA M1
+iek_s_1= 0.0    # PARA M1
 upi_s= 0.0
 up_s = 0.0
 ui_s = 0.0
@@ -87,7 +94,7 @@ k= 0.0
 rk_s2= 0.0
 yk_s2= 0.0
 ek_s2= 0.0
-iek_s2= 0.0        #PARA M2
+iek_s2= 0.0        # PARA M2
 iek_s_12= 0.0   
 upi_s2= 0.0
 up_s2 = 0.0
@@ -100,6 +107,7 @@ setpoint_W = 28
 delta_fn= 0.0
 delta_fn_2= 0.0
 ref= 12.0
+ref2=10.0
 ancho_faja= 3.0
 velocidad =0.0
 
@@ -151,215 +159,165 @@ def control_motor(pin_pwm, pin_dir, speed_percent, direction):
     else:
         raise ValueError("Dirección no válida. Usa 'forward' o 'backward'.")
 
+def check(lon, lat):
+    # build a shapely point from your geopoint
+    point = Point(lon, lat)
 
-# Loop de Control
-start_time = time.time()
+    # the contains function does exactly what you want
+    return polygon.contains(point)
 
-rk_m= float(ref*ancho_faja*velocidad)
-rk_m2= float(35) #  M2
+
+
 # Habilitar motores
 pi.write(motor1_en_pin, 1)
 pi.write(motor2_en_pin, 1)
 
-delta_W_1= 0.0
-delta_fn_1 =0.0
+# Loop principal de control
+start_time = time.time()
+
+rk_m = float(ref * ancho_faja * velocidad)
+rk_m2 = float(ref2 * ancho_faja * velocidad)  #  M2
+
+delta_W_1 = 0.0
+delta_fn_1 = 0.0
 delta_fn_2 = 0.0
 
-delta_W_12= 0.0
-delta_fn_12 =0.0
+delta_W_12 = 0.0
+delta_fn_12 = 0.0
 delta_fn_22 = 0.0
+
 # Crear el archivo de salida para guardar los datos
 output_file_path = '/home/santiago/Documents/dispensador/dispen/maestro_esclavo_definitivo.txt'
 with open(output_file_path, 'w') as output_file:
     output_file.write("Tiempo \t PWM_1 \t PWM_M2 \t W1 \t W2 \tFlujo \t Flujo2\n")
 
-    start_time = time.time()
+    while time.time()-start_time <= 80:
+        # Lee los datos del GPS
+        newdata = ser.readline().decode('utf-8').strip()
+        # Leer el archivo shapefile
+        poly_file = 'poligono_casona.shp'
+        r = shapefile.Reader(poly_file)
 
-    while(time.time()-start_time <= 60):
-        
+        # Verifica si se recibe una sentencia GPRMC  
+        if newdata[0:6] == "$GPRMC":  
+            newmsg = pynmea2.parse(newdata)  
+            status = newmsg.status   
+            # Maneja los estados A y V  
+            if status == "A":  
+                lat = newmsg.latitude  
+                lon = newmsg.longitude  
+                gps = f"Lat = {lat} Lng = {lon}"  
+                print(gps)  
+                speed = newmsg.spd_over_grnd  # velocidad en nudos  
+                velocidad = speed * 0.514444  # convertimos de nudos a m/s  
+                print(f"Speed: {speed:.2f} knots / {velocidad:.2f} m/s")  
+
+                # get the shapes
+                shapes = r.shapes()
+                for k in range(len(shapes)):
+                    # build a shapely polygon from your shape
+                    polygon = shape(shapes[k])    
+                    zone_def = check(lon, lat)
+                    if zone_def:
+                        zone = k
+                        print('El punto corresponde a la zona ' + str(zone+1))
+                    else:
+                        print("Estas Fuera de Rango")
+            elif status == "V":  
+                print("Estoy Agarrando Señal, Krnal ...")  
+                print("Estoy Cansado, Jefe")
+
         t1 = TicToc()  
         t1.tic()   
         k += 1       # Tic
         k2 += 1
 
         flancos_totales_1 = numero_flancos_A + numero_flancos_B
-        RPS = flancos_totales_1 / (600.0)
+        RPS = flancos_totales_1 / 600.0
         W = RPS * ((2 * pi_m) / INTERVALO)
         print("Velocidad: " + str(W))
 
         flancos_totales_2 = numero_flancos_A2 + numero_flancos_B2
-        RPS2 = flancos_totales_1 / (600.0)
+        RPS2 = flancos_totales_2 / 600.0
         W2 = RPS2 * ((2 * pi_m) / INTERVALO)
-        print("Velocidad: " + str(W2))
+        print("Velocidad M2: " + str(W2))
 
-        #Msoft sensor 
-        delta_W = W - setpoint_W
+        delta_fn = flancos_totales_1
+        delta_W = W
 
-        delta_fn= 0.1969*delta_W_1 + 1.359 * delta_fn_1 - 0.581*delta_fn_2 
+        delta_fn2 = flancos_totales_2
+        delta_W2 = W2
 
-        if k <= 3:
-             fm_n= 0
-        else :
-             fm_n = delta_fn + setpoint_f            # PARA M1
+        fm_n = flancos_totales_1 + 180
+        fm_n2 = flancos_totales_2 + 160
+        rk_m = 20
+        rk_m2 = 15
 
-        if k == 60:
-            rk_m= 5
-        if k == 110:
-            rk_m= 45
-        if k == 160:
-            rk_m= 15
+        ek_m = fm_n - W                   # Error de velocidad
+        up_m = ek_m * Kp_m
+        iek_m = iek_m_1 + ek_m            # Error integral
+        ui_m = ki_m * iek_m * INTERVALO
+        upi_m = up_m + ui_m
 
-#-------------------------------------------------------------------------------------------------
-        delta_W2 = W2 - setpoint_W
+        ek_m2 = fm_n2 - W2                # Error de velocidad
+        up_m2 = ek_m2 * Kp_m2
+        iek_m2 = iek_m2 + ek_m2           # Error integral
+        ui_m2 = ki_m2 * iek_m2 * INTERVALO
+        upi_m2 = up_m2 + ui_m2
 
-        delta_fn2= 0.1969*delta_W_12 + 1.359 * delta_fn_12 - 0.581*delta_fn_22 
-
-        if k2 <= 3:
-             fm_n2= 0
-        else :                                                          # Softsensor PARA M2
-             fm_n2 = delta_fn2 + setpoint_f
-
-        if k2 == 60:
-            rk_m2= 5
-        if k2 == 110:
-            rk_m2= 45
-        if k2 == 160:
-            rk_m2= 15
-#----------------------------------------------------------------------------------------------------
-
-
-        #Control maestro para M1
-        yk_m = fm_n
-        ek_m= rk_m - yk_m
-        iek_m = ek_m + iek_m_1
-        up_m = Kp_m*ek_m
-        ui_m = ki_m*iek_m
-        upi_m = up_m  + ui_m
-
-        if upi_m < 0 or upi_m > 100:
-            if upi_m < 0 :
-                ui_m = 0 - up_m
-            if upi_m >100:
-                ui_m = 100 - up_m
-
-        upi_m = ui_m  + up_m
-        print("upi_m = "+ str(upi_m))
-
-
-        #Control maestro para M2
-        yk_m2 = fm_n2
-        ek_m2= rk_m2 - yk_m2
-        iek_m2 = ek_m2 + iek_m_12
-        up_m2 = Kp_m2*ek_m2
-        ui_m2 = ki_m2*iek_m2
-        upi_m2 = up_m2  + ui_m2
-
-        if upi_m2 < 0 or upi_m2 > 100:
-            if upi_m2 < 0 :
-                ui_m2 = 0 - up_m2
-            if upi_m2 >100:
-                ui_m2 = 100 - up_m2
-
-        upi_m2 = ui_m2  + up_m2
-        print("upi_m motor 2= "+ str(upi_m2))
-
-        #Control esclavo para M1
-        rk_s = upi_m
-        yk_s = W
-        ek_s= rk_s - yk_s
-        iek_s = ek_s + iek_s_1
-        ui_s = ki_s*iek_s
-        up_s= kp_s*ek_s
+        ek_s = rk_s - W                   # Error de velocidad
+        up_s = ek_s * kp_s
+        iek_s = iek_s_1 + ek_s            # Error integral
+        ui_s = ki_s * iek_s * INTERVALO
         upi_s = up_s + ui_s
 
-        if upi_s < 0 or upi_s > 100:
-            if upi_s < 0 :
-                ui_s = 0 - up_s
-            if upi_s >100:
-                ui_s = 100 - up_s
-
-        upi_s = ui_s + up_s
-
-
-        print("rks = "+ str(rk_s))
-        print("pwm = "+ str(upi_s))
-        print("flujo = "+ str(fm_n))
-
-
-        #Control esclavo para M2
-
-        rk_s2 = upi_m2
-        yk_s2 = W2
-        ek_s2= rk_s2 - yk_s2
-        iek_s2 = ek_s2 + iek_s_12
-        ui_s2 = ki_s2*iek_s2
-        up_s2= kp_s2*ek_s2
+        ek_s2 = rk_s2 - W2                # Error de velocidad
+        up_s2 = ek_s2 * kp_s2
+        iek_s2 = iek_s2 + ek_s2           # Error integral
+        ui_s2 = ki_s2 * iek_s2 * INTERVALO
         upi_s2 = up_s2 + ui_s2
 
-        if upi_s2 < 0 or upi_s2 > 100:
-            if upi_s2 < 0 :
-                ui_s2 = 0 - up_s2
-            if upi_s2 >100:
-                ui_s2 = 100 - up_s2
+        v1 = upi_s + upi_m
+        v2 = upi_s2 + upi_m2
 
-        upi_s2 = ui_s2 + up_s2
+        print("V1: " + str(v1))
+        print("V2: " + str(v2))
 
+        if v1 <= 0:
+            v1 = 0
+        elif v1 > 48:
+            v1 = 48
+        if v2 <= 0:
+            v2 = 0
+        elif v2 > 48:
+            v2 = 48
 
-        print("rks2 = "+ str(rk_s2))
-        print("pwm2 = "+ str(upi_s2))
-        print("flujo2 = "+ str(fm_n2))
+        # Control del motor 1
+        speed_percent_m1 = v1 * 100 / 48  # Convierte v1 a porcentaje
+        control_motor(motor1_pwm_pin, motor1_dir_pin, speed_percent_m1, 'forward')
 
-        
+        # Control del motor 2
+        speed_percent_m2 = v2 * 100 / 48  # Convierte v2 a porcentaje
+        control_motor(motor2_pwm_pin, motor2_dir_pin, speed_percent_m2, 'forward')
 
-        motor_speed = upi_s  # Asegurar que motor1_speed esté en el rango 0-100
-        control_motor(motor1_pwm_pin, motor1_dir_pin, motor_speed, 'forward')
-        motor2_speed = upi_s2  # Asegurar que motor1_speed esté en el rango 0-100
-        control_motor(motor2_pwm_pin, motor2_dir_pin, motor2_speed, 'forward')
-
-#       Reemplazo Variables m1
+        # Actualiza valores anteriores
         delta_fn_2 = delta_fn_1
         delta_fn_1 = delta_fn
         delta_W_1 = delta_W
 
-        iek_m_1 = iek_m
-
-        iek_s_1 = iek_s
-
-#       Reemplazo Variables m2
         delta_fn_22 = delta_fn_12
         delta_fn_12 = delta_fn2
         delta_W_12 = delta_W2
 
-        iek_m_12 = iek_m2
+        iek_m_1 = iek_m
+        iek_s_1 = iek_s
 
-        iek_s_12 = iek_s2
+        t1.toc()
+        time.sleep(0.2)
 
-        
-        # Registrar los datos en el archivo
-        ts = time.time() - start_time
-        output_file.write(f"{ts:.2f}\t{upi_s:.2f}\t{upi_s2:.2f}\t{W:.2f}\t{W2:.2f}\t{fm_n}\t{fm_n2:.2f}")
+        # Guardar en el archivo de salida
+        output_file.write(f"{time.time() - start_time} \t {v1} \t {v2} \t {W} \t {W2} \t {fm_n} \t {fm_n2}\n")
 
-
-        # Restablecer contadores
-        numero_flancos_A = 0
-        numero_flancos_B = 0
-        numero_flancos_A2 = 0
-        numero_flancos_B2 = 0
-
-        e_time= t1.tocvalue()
-        toc= abs(INTERVALO- e_time)
-        time.sleep(toc)
-        
-        
-    
 # Deshabilitar motores
-pi.set_PWM_dutycycle(motor1_pwm_pin, 0)
-pi.set_PWM_dutycycle(motor2_pwm_pin, 0)
 pi.write(motor1_en_pin, 0)
 pi.write(motor2_en_pin, 0)
-
-
-
-# Detener Pigpio
-pi.stop()
-print('Tiempo de funcionamiento de los motores completado.')
