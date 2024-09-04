6f44a5e69ffdbaf9340388a5dd9c9dffb4730deb
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-09-04 18:01:41 -0500

    BETA MASTER-SLAVE COMPLETO

--- control-gps-pid.py
+++ control-gps-pid.py
@@ -111,6 +111,7 @@ delta_fn_2= 0.0
 v1 = 0.0
 v2 = 0.0
 
+rate= 0.0
 # Configuración de pines de entrada para los encoders
 pi.set_mode(PIN_ENCODER_A, pigpio.INPUT)
 pi.set_pull_up_down(PIN_ENCODER_A, pigpio.PUD_UP)
@@ -163,11 +164,38 @@ def check(lon, lat):
     return polygon.contains(point)
 
 
+while True:
+
+    newdata = ser.readline().decode('utf-8').strip()  
+    
+    # Verifica si se recibe una sentencia GPRMC  
+    if newdata[0:6] == "$GPRMC":  
+        newmsg = pynmea2.parse(newdata)  
+        status = newmsg.status   
+        # Maneja los estados A y V  
+        if status == "A":  
+            lat = newmsg.latitude  
+            lon = newmsg.longitude  
+            gps = f"Lat = {lat} Lng = {lon}"  
+            print(gps)  
+            speed = newmsg.spd_over_grnd  # velocidad en nudos  
+            speed_mps = speed * (0.514444)  # convertimos de nudos a m/s  
+            print(f"Speed: {speed:.2f} knots / {speed_mps:.2f} m/s")
+            break
+
+        elif status == "V":  
+            print("Buscando señal . . .")
+
+speed_mps=0.0
+d=3.6
+
+
+
 # Loop de Control
 start_time = time.time()
 
-rk_m= float(35)
-rk_m2= float(35) #  M2
+rk_m= float(speed_mps*d*rate)
+rk_m2= float(speed_mps*d*rate) #  M2
 # Habilitar motores
 pi.write(motor1_en_pin, 1)
 pi.write(motor2_en_pin, 1)
@@ -175,7 +203,7 @@ pi.write(motor2_en_pin, 1)
 delta_W_1= 0.0
 delta_fn_1 =0.0
 delta_fn_2 = 0.0
-
+contador=0
 delta_W_12= 0.0
 delta_fn_12 =0.0
 delta_fn_22 = 0.0
@@ -183,228 +211,243 @@ delta_fn_22 = 0.0
 output_file_path = '/home/santiago/Documents/dispensador/dispen/uwurrr.txt'
 with open(output_file_path, 'w') as output_file:
     output_file.write("Tiempo \t PWM_1 \t PWM_M2 \t W1 \t W2 \tFlujo \t Flujo2\n")
+    start_time = time.time()
 
-    while(time.time()-start_time <= 60):
+    while(True):
+        
+        t1 = TicToc()       # Tic
+        t1.tic()
+        k += 1
+        gk +=1
 
-        while True:
-            newdata = ser.readline().decode('utf-8').strip()  
-            # read your shapefile
-            poly_file='poligono_casona.shp'
-            r = shapefile.Reader(poly_file)
-
-            if newdata[0:6] == "$GPRMC":  
+        if gk == 3:
+            newdata = ser.readline().decode('utf-8').strip()
+            if newdata[0:6] == "$GPRMC":
                 newmsg = pynmea2.parse(newdata)  
-                status = newmsg.status   
-            # Maneja los estados A y V  
-                if status == "A":  
-                    lat = newmsg.latitude  
-                    lon = newmsg.longitude  
-                    gps = f"Lat = {lat} Lng = {lon}"  
-                    print(gps)  
-                    speed = newmsg.spd_over_grnd  # velocidad en nudos  
+                status = newmsg.status
+                # read your shapefile
+                poly_file='poligono_casona.shp'
+                r = shapefile.Reader(poly_file)
+                # Maneja los estados A y V
+                if status == "A":
+                    lat = newmsg.latitude
+                    lon = newmsg.longitude
+                    gps = f"Lat = {lat} Lng = {lon}"
+                    print(gps)
+                    speed = newmsg.spd_over_grnd  # velocidad en nudos
                     speed_mps = speed * (0.514444)  # convertimos de nudos a m/s  
-                    print(f"Speed: {speed:.2f} knots / {speed_mps:.2f} m/s")  
-
+                    print(f"Speed: {speed_mps:.2f} m/s")
                     # get the shapes
                     shapes = r.shapes()
-                    inside_zone = False  # Bandera para verificar si está dentro de alguna zona
+                    inside_zone = False # Bandera para verificar si está dentro de alguna zona
                     for j in range(len(shapes)):
-                        # build a shapely polygon from your shape
-                        polygon = shape(shapes[j])    
-                        zone_def = check(lon, lat)
+                        polygon = shape(shapes[j])
+                        zone_def = check(lon,lat)
                         if zone_def:
                             zone = j
-                            print('El punto corresponde a la zona ' + str(zone+1))
+                            zona = zone+1
                             inside_zone = True
-                            break   # Sal del bucle si se encuentra una zona
+                            if zona == 1:
+                                rate = 8
+                                print(rate)
+                            if zona == 2:
+                                rate = 10
+                                print(rate)
+                            break
                     
-                    if not inside_zone:
-                        print("Estas Fuera de Rango")
-                        
-                    # Salir del bucle while True
-                    break    
+                    while not inside_zone:
+                        print("Estas Fuera del Aerea a implementar . . .")
+                        rk = 0.0
+                        control_motor(motor1_pwm_pin, motor1_dir_pin, 0, 'forward')
+                        newdata = ser.readline().decode('utf-8').strip()
+                        if newdata[0:6] == "$GPRMC":
+                            newmsg = pynmea2.parse(newdata)  
+                            status = newmsg.status
+                            # read your shapefile
+                            poly_file='poligono_casona.shp'
+                            r = shapefile.Reader(poly_file)
+                            # Maneja los estados A y V
+                            if status == "A":
+                                lat = newmsg.latitude
+                                lon = newmsg.longitude
+                                gps = f"Lat = {lat} Lng = {lon}"
+                                print(gps)
+                                # get the shapes
+                                shapes = r.shapes()
+                                inside_zone = False # Bandera para verificar si está dentro de alguna zona
+                                for j in range(len(shapes)):
+                                    polygon = shape(shapes[j])
+                                    zone_def = check(lon,lat)
+                                    if zone_def:
+                                        zone = j
+                                        zona = zone+1
+                                        inside_zone = True
+                                        if zona == 1:
+                                            rate = 8
+                                            print(rate)
+                                        if zona == 2:
+                                            rate = 10
+                                            print(rate)
+                                        break
 
-                elif status == "V":  
-                    print("Estoy Agarrando Señal, Krnal ...")  
-                    print("Estoy Cansado, Jefe")
-                    
-
-            t1 = TicToc()  
-            t1.tic()   
-            k += 1       # Tic
-            k2 += 1
-
-            flancos_totales_1 = numero_flancos_A + numero_flancos_B
-            RPS = flancos_totales_1 / (600.0)
-            W = RPS * ((2 * pi_m) / INTERVALO)
-            print("Velocidad: " + str(W))
-
-            flancos_totales_2 = numero_flancos_A2 + numero_flancos_B2
-            RPS2 = flancos_totales_1 / (600.0)
-            W2 = RPS2 * ((2 * pi_m) / INTERVALO)
-            print("Velocidad: " + str(W2))
-
-            #Msoft sensor 
-            delta_W = W - setpoint_W
-
-            delta_fn= 0.1969*delta_W_1 + 1.359 * delta_fn_1 - 0.581*delta_fn_2 
-
-            if k <= 3:
-                fm_n= 0
-            else :
-                fm_n = delta_fn + setpoint_f            # PARA M1
-
-            if k == 60:
-                rk_m= 5
-            if k == 110:
-                rk_m= 45
-            if k == 160:
-                rk_m= 15
-
-    #-------------------------------------------------------------------------------------------------
-            delta_W2 = W2 - setpoint_W
-
-            delta_fn2= 0.1969*delta_W_12 + 1.359 * delta_fn_12 - 0.581*delta_fn_22 
-
-            if k2 <= 3:
-                fm_n2= 0
-            else :                                                          # Softsensor PARA M2
-                fm_n2 = delta_fn2 + setpoint_f
-
-            if k2 == 60:
-                rk_m2= 5
-            if k2 == 110:
-                rk_m2= 45
-            if k2 == 160:
-                rk_m2= 15
-    #----------------------------------------------------------------------------------------------------
+                        time.sleep(0.2)
+            gk=0
 
 
-            #Control maestro para M1
-            yk_m = fm_n
-            ek_m= rk_m - yk_m
-            iek_m = ek_m + iek_m_1
-            up_m = Kp_m*ek_m
-            ui_m = ki_m*iek_m
-            upi_m = up_m  + ui_m
+        flancos_totales_1 = numero_flancos_A + numero_flancos_B
+        RPS = flancos_totales_1 / (600.0)
+        W = RPS * ((2 * pi_m) / INTERVALO)
+        print("Velocidad: " + str(W))
 
-            if upi_m < 0 or upi_m > 100:
-                if upi_m < 0 :
-                    ui_m = 0 - up_m
-                if upi_m >100:
-                    ui_m = 100 - up_m
+        flancos_totales_2 = numero_flancos_A2 + numero_flancos_B2
+        RPS2 = flancos_totales_1 / (600.0)
+        W2 = RPS2 * ((2 * pi_m) / INTERVALO)
+        print("Velocidad: " + str(W2))
 
-            upi_m = ui_m  + up_m
-            print("upi_m = "+ str(upi_m))
+        #Msoft sensor 
+        delta_W = W - setpoint_W
 
+        delta_fn= 0.1969*delta_W_1 + 1.359 * delta_fn_1 - 0.581*delta_fn_2 
 
-            #Control maestro para M2
-            yk_m2 = fm_n2
-            ek_m2= rk_m2 - yk_m2
-            iek_m2 = ek_m2 + iek_m_12
-            up_m2 = Kp_m2*ek_m2
-            ui_m2 = ki_m2*iek_m2
-            upi_m2 = up_m2  + ui_m2
+        if k <= 3:
+            fm_n= 0
+        else :
+            fm_n = delta_fn + setpoint_f            # PARA M1
 
-            if upi_m2 < 0 or upi_m2 > 100:
-                if upi_m2 < 0 :
-                    ui_m2 = 0 - up_m2
-                if upi_m2 >100:
-                    ui_m2 = 100 - up_m2
+#-------------------------------------------------------------------------------------------------
+        delta_W2 = W2 - setpoint_W
 
-            upi_m2 = ui_m2  + up_m2
-            print("upi_m motor 2= "+ str(upi_m2))
+        delta_fn2= 0.1969*delta_W_12 + 1.359 * delta_fn_12 - 0.581*delta_fn_22 
 
-            #Control esclavo para M1
-            rk_s = upi_m
-            yk_s = W
-            ek_s= rk_s - yk_s
-            iek_s = ek_s + iek_s_1
-            ui_s = ki_s*iek_s
-            up_s= kp_s*ek_s
-            upi_s = up_s + ui_s
-
-            if upi_s < 0 or upi_s > 100:
-                if upi_s < 0 :
-                    ui_s = 0 - up_s
-                if upi_s >100:
-                    ui_s = 100 - up_s
-
-            upi_s = ui_s + up_s
+        if k2 <= 3:
+            fm_n2= 0
+        else :                                                          # Softsensor PARA M2
+            fm_n2 = delta_fn2 + setpoint_f
+#----------------------------------------------------------------------------------------------------
 
 
-            print("rks = "+ str(rk_s))
-            print("pwm = "+ str(upi_s))
-            print("flujo = "+ str(fm_n))
+        #Control maestro para M1
+        yk_m = fm_n
+        ek_m= rk_m - yk_m
+        iek_m = ek_m + iek_m_1
+        up_m = Kp_m*ek_m
+        ui_m = ki_m*iek_m
+        upi_m = up_m  + ui_m
+
+        if upi_m < 0 or upi_m > 100:
+            if upi_m < 0 :
+                ui_m = 0 - up_m
+            if upi_m >100:
+                ui_m = 100 - up_m
+
+        upi_m = ui_m  + up_m
+        print("upi_m = "+ str(upi_m))
 
 
-            #Control esclavo para M2
+        #Control maestro para M2
+        yk_m2 = fm_n2
+        ek_m2= rk_m2 - yk_m2
+        iek_m2 = ek_m2 + iek_m_12
+        up_m2 = Kp_m2*ek_m2
+        ui_m2 = ki_m2*iek_m2
+        upi_m2 = up_m2  + ui_m2
 
-            rk_s2 = upi_m2
-            yk_s2 = W2
-            ek_s2= rk_s2 - yk_s2
-            iek_s2 = ek_s2 + iek_s_12
-            ui_s2 = ki_s2*iek_s2
-            up_s2= kp_s2*ek_s2
-            upi_s2 = up_s2 + ui_s2
+        if upi_m2 < 0 or upi_m2 > 100:
+            if upi_m2 < 0 :
+                ui_m2 = 0 - up_m2
+            if upi_m2 >100:
+                ui_m2 = 100 - up_m2
 
-            if upi_s2 < 0 or upi_s2 > 100:
-                if upi_s2 < 0 :
-                    ui_s2 = 0 - up_s2
-                if upi_s2 >100:
-                    ui_s2 = 100 - up_s2
+        upi_m2 = ui_m2  + up_m2
+        print("upi_m motor 2= "+ str(upi_m2))
 
-            upi_s2 = ui_s2 + up_s2
+        #Control esclavo para M1
+        rk_s = upi_m
+        yk_s = W
+        ek_s= rk_s - yk_s
+        iek_s = ek_s + iek_s_1
+        ui_s = ki_s*iek_s
+        up_s= kp_s*ek_s
+        upi_s = up_s + ui_s
+
+        if upi_s < 0 or upi_s > 100:
+            if upi_s < 0 :
+                ui_s = 0 - up_s
+            if upi_s >100:
+                ui_s = 100 - up_s
+
+        upi_s = ui_s + up_s
+
+
+        print("rks = "+ str(rk_s))
+        print("pwm = "+ str(upi_s))
+        print("flujo = "+ str(fm_n))
+
+
+        #Control esclavo para M2
+
+        rk_s2 = upi_m2
+        yk_s2 = W2
+        ek_s2= rk_s2 - yk_s2
+        iek_s2 = ek_s2 + iek_s_12
+        ui_s2 = ki_s2*iek_s2
+        up_s2= kp_s2*ek_s2
+        upi_s2 = up_s2 + ui_s2
+
+        if upi_s2 < 0 or upi_s2 > 100:
+            if upi_s2 < 0 :
+                ui_s2 = 0 - up_s2
+            if upi_s2 >100:
+                ui_s2 = 100 - up_s2
+
+        upi_s2 = ui_s2 + up_s2
+
+
+        print("rks2 = "+ str(rk_s2))
+        print("pwm2 = "+ str(upi_s2))
+        print("flujo2 = "+ str(fm_n2))
+
+        
+
+        motor_speed = upi_s  # Asegurar que motor1_speed esté en el rango 0-100
+        control_motor(motor1_pwm_pin, motor1_dir_pin, motor_speed, 'forward')
+        motor2_speed = upi_s2  # Asegurar que motor1_speed esté en el rango 0-100
+        control_motor(motor2_pwm_pin, motor2_dir_pin, motor2_speed, 'forward')
+
+#       Reemplazo Variables m1
+        delta_fn_2 = delta_fn_1
+        delta_fn_1 = delta_fn
+        delta_W_1 = delta_W
+
+        iek_m_1 = iek_m
+
+        iek_s_1 = iek_s
+
+#       Reemplazo Variables m2
+        delta_fn_22 = delta_fn_12
+        delta_fn_12 = delta_fn2
+        delta_W_12 = delta_W2
+
+        iek_m_12 = iek_m2
+
+        iek_s_12 = iek_s2
 
 
         
-            print("eks2 = "+ str(ek_s2))
-            print("ekm2 = "+ str(ek_m2))
-            print("rks2 = "+ str(rk_s2))
-            print("pwm2 = "+ str(upi_s2))
-            print("flujo2 = "+ str(fm_n2))
-
-            
-
-            motor_speed = upi_s  # Asegurar que motor1_speed esté en el rango 0-100
-            control_motor(motor1_pwm_pin, motor1_dir_pin, motor_speed, 'forward')
-            motor2_speed = upi_s2  # Asegurar que motor1_speed esté en el rango 0-100
-            control_motor(motor2_pwm_pin, motor2_dir_pin, motor2_speed, 'forward')
-
-    #       Reemplazo Variables m1
-            delta_fn_2 = delta_fn_1
-            delta_fn_1 = delta_fn
-            delta_W_1 = delta_W
-
-            iek_m_1 = iek_m
-
-            iek_s_1 = iek_s
-
-    #       Reemplazo Variables m2
-            delta_fn_22 = delta_fn_12
-            delta_fn_12 = delta_fn2
-            delta_W_12 = delta_W2
-
-            iek_m_12 = iek_m2
-
-            iek_s_12 = iek_s2
+        # Registrar los datos en el archivo
+        ts = time.time() - start_time
+        output_file.write(f"{ts:.2f}\t{upi_s:.2f}\t{upi_s2:.2f}\t{W:.2f}\t{W2:.2f}\t{fm_n}\t{fm_n2:.2f}")
 
 
-            
-            # Registrar los datos en el archivo
-            ts = time.time() - start_time
-            output_file.write(f"{ts:.2f}\t{upi_s:.2f}\t{upi_s2:.2f}\t{W:.2f}\t{W2:.2f}\t{fm_n}\t{fm_n2:.2f}\t{wg}")
+        # Restablecer contadores
+        numero_flancos_A = 0
+        numero_flancos_B = 0
+        numero_flancos_A2 = 0
+        numero_flancos_B2 = 0
 
-
-            # Restablecer contadores
-            numero_flancos_A = 0
-            numero_flancos_B = 0
-            numero_flancos_A2 = 0
-            numero_flancos_B2 = 0
-
-            e_time= t1.tocvalue()
-            toc= abs(INTERVALO- e_time)
-            time.sleep(toc)
+        e_time= t1.tocvalue()
+        toc= abs(INTERVALO- e_time)
+        time.sleep(toc)
         
         
     
