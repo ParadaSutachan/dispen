58dfcb57d5c8a25f421a1e493a8e5ef9ae472eba
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-09-06 15:41:09 -0500

    .

--- main.py
+++ main.py
@@ -12,17 +12,17 @@ def test_esc():
     print("Probando el ESC...")
 
     # Enviar señal intermedia (1500 us, punto neutro)
-    pi.set_servo_pulsewidth(ESC_PIN, 1500)
+    pi.set_servo_pulsewidth(ESC_PIN, 1010)
     print("Señal neutra (1500 us)...")
     time.sleep(10)
 
     # Enviar señal máxima (2000 us)
-    pi.set_servo_pulsewidth(ESC_PIN, 1050)
+    pi.set_servo_pulsewidth(ESC_PIN, 1020)
     print("Señal máxima (2000 us)...")
     time.sleep(10)
 
     # Detener motor con señal mínima (1000 us)
-    pi.set_servo_pulsewidth(ESC_PIN, 1100)
+    pi.set_servo_pulsewidth(ESC_PIN, 1030)
     print("Señal mínima (1000 us)...")
     time.sleep(10)
 
