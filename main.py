b05a34a7f74f7b7caf55ca38a1c9ee33aa107f31
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-09-06 15:50:21 -0500

    .

--- main.py
+++ main.py
@@ -11,9 +11,13 @@ ESC_PIN = 21  # Cambia al pin GPIO que estés utilizando
 def test_esc():
     print("Probando el ESC...")
 
+    pi.set_servo_pulsewidth(ESC_PIN, 2000)
+    print("Señal neutra (2000 us)...")
+    time.sleep(1)
+
     # Enviar señal intermedia (1500 us, punto neutro)
     pi.set_servo_pulsewidth(ESC_PIN, 1050)
-    print("Señal neutra (1500 us)...")
+    print("Señal neutra (1050 us)...")
     time.sleep(10)
 
 
