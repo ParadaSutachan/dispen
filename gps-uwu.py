9dedcea4a5ccebb46a3d32b45595f580c4c96512
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-09-06 17:14:41 -0500

    .

--- gps-uwu.py
+++ gps-uwu.py
@@ -1,32 +1,26 @@
 #!/usr/bin/env python
-import pigpio
+
+# esc_start.py
+# 2015-04-14
+# Public Domain
+#
+# Sends the servo pulses needed to initialise some ESCs
+#
+# Requires the pigpio daemon to be running
+#
+# sudo pigpiod
+
 import time
 
-# Conectarse al daemon pigpio
-pi = pigpio.pi()
+import pigpio
 
-# Definir el pin del ESC
-ESC_PIN = 21  # Cambia al pin GPIO que estés utilizando
+SERVO = 16
 
-# Enviar señal máxima de 2000 microsegundos para el límite superior
-def calibrate_max():
-    print("Enviando señal máxima para calibración...")
-    pi.set_servo_pulsewidth(ESC_PIN, 2000)  # 2000us = señal máxima
-    time.sleep(2)  # Esperar 2 segundos para que el ESC registre el máximo
+pi = pigpio.pi() # Connect to local Pi.
 
-<<<<<<< HEAD
-calibrate_max()
+pi.set_servo_pulsewidth(SERVO, 1050) # Minimum throttle.
 
-# Nota: Mantén encendido durante este tiempo y desconecta la batería del ESC después de 2 segundos.
-=======
-# Volver a la configuración original: de 1000 a 2000 microsegundos
-try:
-    set_pwm_from_microseconds(1600)
-    time.sleep(2)
+time.sleep(10)
 
-    print("Configuración completa.")
-    
-finally:
-    pwm.stop()  # Detener el PWM
-    GPIO.cleanup()  # Limpiar los pines GPIO al finalizar
->>>>>>> 41b0b008625864cba1929379a062040c27cd4ff6
+###
+pi.stop() # Disconnect from local Raspberry Pi.
