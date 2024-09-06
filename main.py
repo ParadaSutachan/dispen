97e19c36825dd23804d70f80e1b8c630d58983fc
Laps <parada.sutachan@gmail.com>
2024-09-06 10:00:43 -0500

    .

--- main.py
+++ main.py
@@ -17,24 +17,12 @@ import pigpio
 SERVO = 21
 
 pi = pigpio.pi() # Connect to local Pi.
+pi.set_mode(SERVO, pigpio.OUTPUT)
+
 
 pi.set_servo_pulsewidth(SERVO, 2000) # Minimum throttle.
 
-input("Press enter . ")
-
-time.sleep(2)
-
-input("Press enter")
-pi.set_servo_pulsewidth(SERVO, 500) # Maximum throttle.
-
-time.sleep(5)
-
-pi.set_servo_pulsewidth(SERVO, 1250) # Slightly open throttle.
-
-time.sleep(5)
-
-pi.set_servo_pulsewidth(SERVO, 0) # Stop servo pulses.
-
 time.sleep(3)
 
+###
 pi.stop() # Disconnect from local Raspberry Pi.
