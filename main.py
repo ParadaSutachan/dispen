8dc0a630f43bc8fd2042db70784ac09524797c8d
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-09-06 15:43:14 -0500

    .

--- main.py
+++ main.py
@@ -16,15 +16,7 @@ def test_esc():
     print("Señal neutra (1500 us)...")
     time.sleep(10)
 
-    # Enviar señal máxima (2000 us)
-    pi.set_servo_pulsewidth(ESC_PIN, 1080)
-    print("Señal máxima (2000 us)...")
-    time.sleep(10)
 
-    # Detener motor con señal mínima (1000 us)
-    pi.set_servo_pulsewidth(ESC_PIN, 1100)
-    print("Señal mínima (1000 us)...")
-    time.sleep(10)
 
 test_esc()
 
