c9dc70fca31e6a98cfde91bd82ec78fc5ff07d35
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-09-06 16:08:18 -0500

    .

--- main.py
+++ main.py
@@ -8,26 +8,20 @@ pi = pigpio.pi()
 # Definir el pin del ESC
 ESC_PIN = 21  # Cambia al pin GPIO que estés utilizando
 
-def test_esc():
-    print("Probando el ESC...")
+# Enviar señal máxima de 2000 microsegundos para el límite superior
+def calibrate_max():
+    print("Enviando señal máxima para calibración...")
+    pi.set_servo_pulsewidth(ESC_PIN, 20000)  # 2000us = señal máxima
+    input("presente") 
+    pi.set_servo_pulsewidth(ESC_PIN, 10000) 
+    time.sleep(5)
+    print("ready pa")
+    
 
-    # Enviar señal intermedia (1500 us, punto neutro)
-    pi.set_servo_pulsewidth(ESC_PIN, 1150)
-    print("Señal neutra (1150 us)...")
-    time.sleep(3)
 
-    # Enviar señal máxima (2000 us)
-    pi.set_servo_pulsewidth(ESC_PIN, 1200)
-    print("Señal máxima (1200 us)...")
-    time.sleep(3)
+calibrate_max()
 
-    # Detener motor con señal mínima (1000 us)
-    pi.set_servo_pulsewidth(ESC_PIN, 1100)
-    print("Señal mínima (1100 us)...")
-    time.sleep(3)
+pi.stop()
+print('Tiempo de funcionamiento de los motores completado.')
 
-test_esc()
-
-# Apagar el servo PWM y limpiar
-pi.set_servo_pulsewidth(ESC_PIN, 0)  # Apagar el PWM
-pi.stop()  # Detener pigpio
+# Nota: Mantén encendido durante este tiempo y desconecta la batería del ESC después de 2 segundos.
