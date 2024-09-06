03b4692736ac60966d7066c065a8e7ca5f0f337e
Laps <parada.sutachan@gmail.com>
2024-09-06 10:27:27 -0500

    .

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ SERVO = 21
 
 pi = pigpio.pi() # Connect to local Pi.
 input("Press enter")
-pi.set_servo_pulsewidth(SERVO, 1300) # Minimum throttle.
+pi.set_servo_pulsewidth(SERVO, 1500) # Minimum throttle.
 time.sleep(10)
 pi.set_servo_pulsewidth(SERVO, 0) # Minimum throttle.
 pi.stop() # Disconnect from local Raspberry Pi.
