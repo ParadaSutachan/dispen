7beb1f6a870e1bd9bfaaca5e37604edabd3f24eb
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-09-06 16:13:09 -0500

    .

--- main.py
+++ main.py
@@ -9,14 +9,13 @@ pi = pigpio.pi()
 ESC_PIN = 21  # Cambia al pin GPIO que estés utilizando
 
 # Enviar señal mínima de 1000 microsegundos para el límite inferior
-def calibrate_min():
-    print("Enviando señal mínima para calibración...")
-    pi.set_servo_pulsewidth(ESC_PIN, 2500)  # 1000us = señal mínima
-    input("presente")
-    pi.set_servo_pulsewidth(ESC_PIN, 500)  # 1000us = señal mínima
-    time.sleep(5)  # Esperar 2 segundos para que el ESC registre el mínimo
 
-calibrate_min()
+print("Enviando señal mínima para calibración...")
+pi.set_servo_pulsewidth(ESC_PIN, 2500)  # 1000us = señal mínima
+input("presente")
+pi.set_servo_pulsewidth(ESC_PIN, 500)  # 1000us = señal mínima
+time.sleep(5)  # Esperar 2 segundos para que el ESC registre el mínimo
+
 # Detener Pigpio
 pi.stop()
 # Nota: Después de esto, el ESC debería estar calibrado para aceptar señales de 1000 a 2000 microsegundos.
