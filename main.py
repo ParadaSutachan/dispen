7e90ae020f3c2a6e04deeea3fb3ce93e42639dd4
Laps <parada.sutachan@gmail.com>
2024-09-06 09:46:17 -0500

    .

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ pi.set_mode(SERVO, pigpio.OUTPUT)
 
 pi.set_PWM_frequency(SERVO, 50)
 pi.set_PWM_range(SERVO, 2000) # 1,000,000 / 50 = 20,000us for 100% duty cycle
-pi.set_servo_pulsewidth(SERVO, 1200) # Minimum throttle.
+pi.set_servo_pulsewidth(SERVO, 500) # Minimum throttle.
 
 time.sleep(10)
 
