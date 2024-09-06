516271b71acbf7de2e9a463f58dc3986f8f6c1f7
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-09-06 16:01:15 -0500

    .

--- main.py
+++ main.py
@@ -11,11 +11,9 @@ ESC_PIN = 21  # Cambia al pin GPIO que estés utilizando
 # Enviar señal máxima de 2000 microsegundos para el límite superior
 def calibrate_max():
     print("Enviando señal máxima para calibración...")
-    pi.set_servo_pulsewidth(ESC_PIN, 2000)  # 2000us = señal máxima
+    pi.set_servo_pulsewidth(ESC_PIN, 1200)  # 2000us = señal máxima
     time.sleep(2)  # Esperar 2 segundos para que el ESC registre el máximo
 
 calibrate_max()
 
-# Detener Pigpio
-pi.stop()
 # Nota: Mantén encendido durante este tiempo y desconecta la batería del ESC después de 2 segundos.
