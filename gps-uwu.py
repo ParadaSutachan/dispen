894aff861956ae11a369896fed7809f143949c11
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-09-06 19:16:36 -0500

    .

--- gps-uwu.py
+++ gps-uwu.py
@@ -12,11 +12,11 @@ ESC_PIN2 = 20  # Cambia al pin GPIO que estés utilizando
 # Enviar señal máxima de 2000 microsegundos para el límite superior
 
 print("Enviando señal máxima para calibración...")
-pi.set_servo_pulsewidth(ESC_PIN, 2000)  # 2000us = señal máxima
+pi.set_servo_pulsewidth(ESC_PIN, 6)  # 2000us = señal máxima
 time.sleep(5)  # Esperar 2 segundos para que el ESC registre el máximo
 
 print("Enviando señal 1200 para calibración...")
-pi.set_servo_pulsewidth(ESC_PIN2, 1200)  # 2000us = señal máxima
+pi.set_servo_pulsewidth(ESC_PIN2, 10)  # 2000us = señal máxima
 time.sleep(5)  # Esperar 2 segundos para que el ESC registre el máximo
 
 
