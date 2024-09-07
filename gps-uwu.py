36b209e2eb0597b8b453610992043f3173d774a2
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-09-06 19:49:06 -0500

    .

--- gps-uwu.py
+++ gps-uwu.py
@@ -5,23 +5,16 @@ import time
 # Conectar al daemon pigpio
 pi = pigpio.pi()
 
-# Verificar que la conexión con el daemon pigpio esté activa
-if not pi.connected:
-    exit()
 
 # Definir el pin GPIO donde está conectado el ESC
 ESC_PIN = 21  # Cambia este valor por el pin GPIO que estés utilizando
 
-# Función para establecer el valor máximo (2000 us)
-def set_max_pwm():
-    print("Estableciendo el valor máximo de 2000 us...")
-    pi.set_servo_pulsewidth(ESC_PIN, 2000)  # Configura el PWM a 2000 us (máximo)
-    time.sleep(5)  # Mantener durante 5 segundos para probar el valor máximo
+print("Estableciendo el valor máximo de 2000 us...")
+pi.set_servo_pulsewidth(ESC_PIN, 2000)  # Configura el PWM a 2000 us (máximo)
+time.sleep(10)  # Mantener durante 5 segundos para probar el valor máximo
 
-try:
-    set_max_pwm()
 
-finally:
-    pi.set_servo_pulsewidth(ESC_PIN, 0)  # Apagar el PWM al finalizar
-    pi.stop()  # Finalizar pigpio
-    print("Proceso completado y conexión con pigpio cerrada.")
+
+pi.set_servo_pulsewidth(ESC_PIN, 0)  # Apagar el PWM al finalizar
+pi.stop()  # Finalizar pigpio
+print("Proceso completado y conexión con pigpio cerrada.")
